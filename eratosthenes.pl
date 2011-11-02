d500be5344bac5bdacfdfe236007ff95eac1e977
Minoru Sakamoto <sakamoto_desugananika@i.softbank.jp>
2011-11-02 17:32:28 +0900

    Eratosthenes method

---
+++ eratosthenes.pl
@@ -0,0 +1,23 @@
+use strict;
+
+my $max = 10000000;
+my @prime = ();
+
+for(0...$max){
+	$prime[$_] = 1;
+}
+
+for (2...$max){
+	if( $prime[$_] != 0 ){
+		my $n = $_;
+		for(my $i = 2;$i*$n <= $max;$i++){
+			$prime[$n*$i]=0;
+		}
+	}
+}
+
+for(2...$max){
+	if($prime[$_] != 0){
+		print "$_\n";
+	}
+}
