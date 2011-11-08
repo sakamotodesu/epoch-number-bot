d6584fa9432c321d63a161f9d099886176e4a161
Minoru Sakamoto <sakamoto_desugananika@i.softbank.jp>
2011-11-08 17:17:35 +0900

    change max

--- eratosthenes.pl
+++ eratosthenes.pl
@@ -1,13 +1,13 @@
 use strict;
 
-my $max = 10000000;
+my $max =  10000000;
 my @prime = ();
 
 for(0...$max){
 	$prime[$_] = 1;
 }
 
-for (2...$max){
+for (2...sqrt($max)){
 	if( $prime[$_] != 0 ){
 		my $n = $_;
 		for(my $i = 2;$i*$n <= $max;$i++){
