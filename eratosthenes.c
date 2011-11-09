e421c88a6c1ac08639ad0598f83a1f0577b86852
Minoru Sakamoto <sakamoto_desugananika@i.softbank.jp>
2011-11-09 21:29:58 +0900

    eratosthenes for big

--- eratosthenes.c
+++ eratosthenes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int get_bit( unsigned char* list, int num );
 void set_bit( unsigned char* list, int num );
@@ -26,26 +27,34 @@ int get_bit( unsigned char* list, int num ){
 }
 
 void set_bit( unsigned char* list, int num ){
-	*( list + ( num / 8 ) ) ^= (unsigned char)( pow2( num % 8 ) ); 
+	*( list + ( num / 8 ) ) |= (unsigned char)( pow2( num % 8 ) ); 
 }
 
 int main(void){
 
-	int max = 100;
+	int max = 1500000000;
 	unsigned char* list = NULL;
 
-	list = (unsigned char*)malloc(max/8+1);
+	list = (unsigned char*)calloc(1,max/8+1);
 	if( list == NULL ){
 		perror("malloc fail");
 		exit(1);
 	}
-	memset(list, 0xFF, max);
 	int i;
-	printf("test\n");
-	set_bit(list,10);
-	set_bit(list,98);
-	for( i = 0; i<max; i++ ){
-		printf("%dbit: %d\n",i,get_bit(list,i));
+	for( i = 2; i < sqrt( max ); i++ ){
+		if ( get_bit( list, i ) == 0 ) {
+			int j;
+			for( j = 2; i * j < max; j++ ){
+				set_bit( list, i * j );
+			}
+		}
 	}
+
+	for ( i = 2; i < max ; i++ ){
+		if ( get_bit( list, i ) == 0 ) {
+			printf("%d\n",i);
+		}
+	}
+
 	return 0;
 }
